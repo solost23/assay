ae07e7781b85f43c05c7e3b167a6e3fa1a79c6fd
solost <tianyuanyuans@163.com>
2024-11-15 15:01:31 +0800

    feat: car_code mqtt

--- services/barriers.go
+++ services/barriers.go
@@ -2,6 +2,9 @@ package services
 
 import (
 	"assay/forms"
+	"assay/infra/constant"
+	"assay/infra/response"
+	"assay/services/servants"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +16,16 @@ type BarriersService struct{}
 func (*BarriersService) Open(c *gin.Context, params *forms.BarriersOpenForm) {
 	// 检查是否是允许通行，此处先都允许通行
 	zap.S().Infof("recv: %+#v from plateNo %s", params, params.PlateNo)
+	// 发送 mqtt
+	publishBarriersForm := servants.PublishBarriersForm{
+		CarCode:   params.PlateNo,
+		CarStatus: 1,
+		TaskId:    1,
+	}
+	if err := servants.PublishBarriers(publishBarriersForm); err != nil {
+		response.Error(c, constant.InternalServerErrorCode, err)
+		return
+	}
 	c.JSON(http.StatusOK, gin.H{
 		"code":    0,
 		"errDesc": "success",
