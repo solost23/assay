f21e4f63b38fad3042b8185197b63d028f2a0ac2
solost <tianyuanyuans@163.com>
2024-12-12 10:18:37 +0800

    feat: signal listen

--- hik/main.cpp
+++ hik/main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <thread>
+#include <atomic>
+#include <csignal>
 
 #include "controllers/nvr.h"
 
-int main() {
+void signaler(int sig);
+void start();
+
+std::atomic<bool> running(true);
+
+int main() 
+{
+    std::signal(SIGINT, signaler);
+    std::signal(SIGTERM, signaler);
+
+    std::thread http_thread(start);
+    spdlog::info("HTTP server start. Press Ctrl+C to stop.");
+
+    while(running) {}
+
+    if (http_thread.joinable()) {
+        http_thread.join();
+    }
+    spdlog::info("HTTP server stoped.");
+
+    return 0;
+}
+
+void signaler(int sig)
+{
+    running = false;
+    spdlog::info("Interrupt signal received");
+}
+
+void start()
+{
     // 读取配置文件
     Config config{"./configs/config.yml"};
 
@@ -19,8 +52,8 @@ int main() {
 
     std::stringstream ss; ss << config.get_port();
     spdlog::info("server start: " + ss.str());
+
     server.listen("0.0.0.0", config.get_port(), config.get_thread());
 
     spdlog::info("server stop");
-    return Error::Nil;
 }
